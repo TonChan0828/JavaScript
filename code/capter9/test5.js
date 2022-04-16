53f4b58702e7e7daa41361506ded683804ad5686
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-04-16 23:19:58 +0900

    9章完了

---
+++ code/capter9/test5.js
@@ -0,0 +1,33 @@
+class User {
+  constructor(username){
+    this.username = username;
+    this.deleted =0;
+  }
+  login(){
+    if(this.deleted === 0){
+      console.log(`${this.username}はログインに成功しました。`);
+    }else{
+      console.log(`${this.username}はログインに失敗しました。`);
+    }
+  }
+}
+
+class AdminUser extends User{
+  constructor(username){
+    super(username);
+  }
+  deleteUser(user){
+    if(!(user instanceof User)){
+      throw new Error("Userオブジェクトを引数にする必要があります。");
+    }else{
+      user.deleted = 1;
+      console.log(`${user.username}を削除しました。`);
+    }
+  }
+}
+
+const user = new User("user1");
+const admin = new AdminUser("user2");
+
+admin.deleteUser(user);
+user.login();
