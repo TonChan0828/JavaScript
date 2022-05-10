ef5818eb369335be210365c88239ffb7873d6296
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-05-11 07:31:34 +0900

    16章完了

---
+++ code/capter16/practice1.js
@@ -0,0 +1,11 @@
+export const obj = {console(){console.log("object(obj)");}};
+
+export function fn(){console.log("function(fn)");}
+
+export class StdClass{
+  static console(){
+    console.log("Class(StdClass");
+  }
+}export default function(){
+  console.log("default export function");
+};
