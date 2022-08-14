36d978d109a405e6aa03bdbd91992d3b4aa120d6
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-08-14 17:40:49 +0900

    Section5-2

--- React/Capter5/next_app/pages/other.js
+++ React/Capter5/next_app/pages/other.js
@@ -1,14 +1,15 @@
 import Layout from '../components/layout';
 import Link from 'next/link';
-import MyImage from '../components/image';
 
 export default function Home() {
   return (
     <div>
-      <Layout header="Next.js" title="Top page.">
+      <Layout header="Next.js" title="Other page.">
       <div className="alert alert-primary text-center">
         <h5 className="mb-4">Welcome to next.js!</h5>
-        <MyImage fname="image.jpg" size="300"/>
+        <Link href="./">
+          <button className="btn btn-primary">&lt;&lt; Back to Top.</button>
+        </Link>
         </div>
         </Layout>
     </div>
