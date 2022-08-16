21aa17b6da64cbae0403ea7bf227b6508a615911
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-08-16 22:50:16 +0900

    Section6-3 データを表示

---
+++ React/Capter6/next_app/components/fire.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+// import firebase from 'firebase';
+import firebase from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAFCGgsO1u7YB-Lc_FWdUJJuJL1MhfoLE",
+  authDomain: "myreactprctice.firebaseapp.com",
+  projectId: "myreactprctice",
+  storageBucket: "myreactprctice.appspot.com",
+  messagingSenderId: "341952626886",
+  appId: "1:341952626886:web:d8678a241e6409b2d997c7"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
